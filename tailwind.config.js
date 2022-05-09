29aa399f5789660947575da5f5e439a83f4f602a
Rolf Geschke <71587437+Natorden@users.noreply.github.com>
2022-05-09 20:41:30 +0200

    Attempted to add Bootstrap and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('flowbite/plugin')
+  ],
+};
